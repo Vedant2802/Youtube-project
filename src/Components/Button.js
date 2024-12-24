2565aad0c45f8b21b3fdfac19f9899979042ddcc
vedant-akshat <103035956+vedant-akshat@users.noreply.github.com>
2024-12-25 05:11:30 +0530

    fix: minor fix

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -3,7 +3,9 @@ import React from "react";
 const Button = () => {
   return (
     <div>
-      <Button classname="px-5 py-2 m-5 rounded-lg bg-gray-400">All</Button>
+      <button className="px-5 py-2 m-5 bg-gray-100 shadow-lg rounded-lg">
+        All
+      </button>
     </div>
   );
 };
